12eb88b8cb643daabf769fb5501972409a1cb1ed
andreylavrusenko <m>
2022-11-23 19:56:23 +0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import '../styles/globals.css'
+import Layout from "./Layout";
+import ProgressBar from "@badrap/bar-of-progress";
+import {Router} from "next/router";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+
+    const progress = new ProgressBar({
+        size: 4,
+        color: "#FE596E",
+        className: 'z-50',
+        delay: 100
+    });
+
+    Router.events.on('routeChangeStart', progress.start)
+    Router.events.on('routeChangeComplete', progress.finish)
+    Router.events.on('routeChangeError', progress.finish)
+
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    )
 }
 
 export default MyApp
